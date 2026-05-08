3cfee704f1f1e65f945ffad560d3836ea8d6fedc
Wezi Zimba <wzizimba@proton.me>
2026-05-08 10:55:21 +0200

    Implement plotting for mathematical functions
    Added functions for plotting and visualizing mathematical expressions.

--- linearization/scripts/approx002.m
+++ linearization/scripts/approx002.m
@@ -19,3 +19,28 @@ disp(Lg);
 fprintf('h(x) -  Lh(x) = ');
 disp(Lh);
 
+ffn=@(t) (t-1).^2;
+gfn=@(t) exp(-2*t);
+hfn=@(t) 1+log(1-2*t);
+Lfn=@(t) 1-2*t;
+
+%                                           Plot
+
+figure(1); 
+clf;
+xp=linspace(-0.6,0.49,600);
+
+plot(xp,ffn(xp), '-','Color',[0 0 0],'LineWidth',1.7,'DisplayName','f(x) = (x-1)^2');
+hold on;
+plot(xp,gfn(xp), '-','Color',[.4 .5 .6],'LineWidth',1.7,'DisplayName','g(x) = exp(-2x)');
+plot(xp,hfn(xp), '-','Color',[.30 .30 .30],'LineWidth',1.7,'DisplayName','h(x) = 1 + lin(1-2x)');
+plot(xp,Lfn(xp), '--','Color',[1 0 0],'LineWidth',1.7,'DisplayName','L(x) = 1-2x');
+
+
+
+
+
+
+
+
+
